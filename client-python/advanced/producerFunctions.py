a14d8c700d5fcfb7dc161e13f87656a7068a7d96
Muhammad Fahry <muhammadfahry18@gmail.com>
2018-01-03 18:02:03 +0700

    advanced producers

---
+++ client-python/advanced/producerFunctions.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Example Kafka Producer.
+# Reads lines from stdin and sends to Kafka.
+#
+
+from confluent_kafka import Producer
+
+class ProducerFunctions:
+    broker = '10.128.0.2:9092'
+    topics = ("transaction", "transactionWithFraud")
+
+    def __init__(self, argv):
+        conf = {'bootstrap.servers': ProducerFunctions.broker}
+        self.producer = Producer(**conf)
+
+    def delivery_callback(self, err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d] @ %o\n' % (msg.topic(), msg.partition(), msg.offset()))
+
+    def send_kafka(self, params, topic):
+        self.producer.produce(topic, params, callback=ProducerFunctions.delivery_callback)
+        self.producer.poll(0)
+
+    def transactionHandling(self, params):
+        self.send_kafka(params, topics[0])
+
+    def transactionWithFraudHandling(self, params):
+        self.send_kafka(params, topics[1])
+
+    def close_kafka(self):
+        self.producer.flush()
