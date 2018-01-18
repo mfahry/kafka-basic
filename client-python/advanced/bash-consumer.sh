82ff4749293cf984a3c501b0d7074bf81374b8c0
Muhammad Fahry <muhammadfahry18@gmail.com>
2018-01-18 16:06:16 +0700

    change to bash file

--- client-python/advanced/bash-consumer.sh
+++ client-python/advanced/bash-consumer.sh
@@ -1,1 +1,1 @@
-python consumer.py 10.128.0.2:9092 test-group transaction
+python consumer_v1.py
--- client-python/advanced/consumer_v1.py
+++ client-python/advanced/consumer_v1.py
@@ -1,25 +1,24 @@
 
 from confluent_kafka import Consumer, KafkaError
-import json
+import json, requests
 
 c = Consumer({'bootstrap.servers': '10.128.0.2:9092', 'group.id': 'mygroup', 'default.topic.config': {'auto.offset.reset': 'smallest'}})
 c.subscribe(['transaction'])
 running = True
 while running:
     msg = c.poll()
-    
+
     if not msg.error():
         jsonString = msg.value().decode('utf-8').replace("'",'"')
-
-        #print(jsonString)
         jsonData =  json.loads(jsonString)
-        print(type(jsonData))
         print(jsonData)
-        #change to FDS microservices
-        #url = 'http://localhost:8080'
-        #data = open('example.json','r')
-        #headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        #r = requests.post(url, data=json.dumps(data), headers=headers)
+
+        # change to FDS microservices
+        # url = 'http://localhost:8080'
+        # data = open('example.json','r')
+        # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        # r = requests.post(url, data=json.dumps(data), headers=headers)
+
     elif msg.error().code() != KafkaError._PARTITION_EOF:
         print(msg.error())
         running = False
