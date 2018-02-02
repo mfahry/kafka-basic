bb7092290dbb93e148ac831d1c9f5967fba0fd3e
Muhammad Fahry <muhammadfahry18@gmail.com>
2018-02-02 16:34:08 +0700

    fix producer schema

--- client-python/advanced/producerFunctionsWithAvro.py
+++ client-python/advanced/producerFunctionsWithAvro.py
@@ -2,7 +2,6 @@ from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 from confluent_kafka.avro.serializer import (KeySerializerError,ValueSerializerError)
 from tests.avro.mock_schema_registry_client import MockSchemaRegistryClient
-from confluent_kafka import Producer
 
 class ProducerFunctions:
     broker = '172.18.133.252:9092'
@@ -11,7 +10,7 @@ class ProducerFunctions:
     def __init__(self):
         conf = {'bootstrap.servers': ProducerFunctions.broker}
         self.producer = Producer(**conf)
-        value_schema = avro.load("schema.avsc"))
+        value_schema = avro.load("schema.avsc")
         producer = AvroProducer({'schema.registry.url': 'http://172.18.133.252:9001'}, default_value_schema=value_schema)
 
     def delivery_callback(self, err, msg):
